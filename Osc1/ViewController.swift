cd777589cb13c891f94359ffdf5c61ef233501ba
Vinicius Mello <vinicius.mello@ufba.br>
2015-05-03 16:53:02 -0300

    Initial Commit

---
+++ Osc1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Osc1
+//
+//  Created by Vinicius Mello on 03/05/15.
+//  Copyright (c) 2015 Vinicius Mello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
